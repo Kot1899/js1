9c739d3a3774668d7e2be70062bb015eb2499cf1
Vitali Roma <romanenko.vit.kharkiv@gmail.com>
2024-04-26 15:21:08 +0300

    числа

---
+++ M1_number.js
@@ -0,0 +1,16 @@
+
+const valueA = "5";
+console.log(Number(valueA));// 5
+
+// parseInt парсить і повертає ціле число
+console.log(Number.parseInt("5px")); // 5
+console.log(Number.parseInt("12qwe74")); // 12
+console.log(Number.parseInt("12.46qwe79")); // 12
+console.log(Number.parseInt("qweqwe")); // NaN
+
+// parseFloat  парсить і повертає дробове число
+console.log(Number.parseFloat("5px")); // 5
+console.log(Number.parseFloat("12qwe74")); // 12
+console.log(Number.parseFloat("12.46qwe79")); // 12.46
+console.log(Number.parseFloat("qweqwe")); // NaN
+
