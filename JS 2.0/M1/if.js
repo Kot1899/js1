89ce4c60d9000119b47250e7217b4d92ca50238f
Vitali Roma <romanenko.vit.kharkiv@gmail.com>
2024-05-09 16:56:27 +0300

    Anna index

--- JS 2.0/M1/if.js
+++ JS 2.0/M1/if.js
@@ -3,10 +3,14 @@
 let price =0;
 const name = "pro";
 if(name==="pro"){
-    price=100;
+    price=10040;
 }
 
 console.log(price);// 100
+document.write(price);
+
+
+/*
 
 //when false
 let price2 =0;
@@ -42,3 +46,39 @@ console.log("invalid")
 }
 
 console.log(number);// 300
+
+*/
+
+
+// function checkAge(age) {
+//     let message;
+  
+//     if (age >= 18) { // Change this line
+//       message = 'You are an adult';
+//     } else {
+//       message = 'You are a minor';
+//     }
+  
+//     return message;
+//   }
+  
+  
+  function checkAge(age) {
+  let message;
+    if (age >=18 ){
+        
+        message = "You are an adult";
+             }else {
+        message = "You are a minor";
+          }
+    // return message;
+
+    document.write(message);
+    document.write('<br/>');
+  };
+
+  checkAge(20);
+    checkAge(8);
+    checkAge(14);
+    checkAge(38);
+  
