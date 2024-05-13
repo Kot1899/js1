323a29349254a95fd9e4e26070a98e2e9386e172
Vitali Roma <romanenko.vit.kharkiv@gmail.com>
2024-05-13 18:18:21 +0300

    mon

---
+++ JS 2.0/M1/variableScope.js
@@ -0,0 +1,52 @@
+const global = "global";
+
+if (true) {
+  const blockA = "block A";
+
+  // Бачимо глобальну + локальну A
+  console.log(global); // 'global'
+  console.log(blockA); // block A
+
+  // Змінні blockB і blockC не знайдені в доступних областях видимості.
+  // Буде помилка звернення до змінної.
+  console.log(blockB); // ReferenceError: blockB is not defined
+  console.log(blockC); // ReferenceError: blockC is not defined
+
+  if (true) {
+    const blockB = "block B";
+
+    // Бачимо глобальну + зовнішню A + локальну B
+    console.log(global); // global
+    console.log(blockA); // block A
+    console.log(blockB); // block B
+
+    // Змінна blockC не знайдена в доступних областях видимості.
+    // Буде помилка звернення до змінної.
+    console.log(blockC); // ReferenceError: blockC is not defined
+  }
+}
+
+
+
+
+if (true) {
+  const blockC = "block C";
+
+  // Бачимо глобальну + локальну C
+  console.log(global); // global
+  console.log(blockC); // block C
+
+  // Змінні blockA і blockB не знайдені в доступних областях видимості.
+  // Буде помилка звернення до змінної.
+  console.log(blockA); // ReferenceError: blockA is not defined
+  console.log(blockB); // ReferenceError: blockB is not defined
+}
+
+// Бачимо лише глобальну
+console.log(global); // global
+
+// Змінні blockA, blockB і blockC не знайдені в доступних областях видимості.
+// Буде помилка звернення до змінної.
+console.log(blockA); // ReferenceError: blockA is not defined
+console.log(blockB); // ReferenceError: blockB is not defined
+console.log(blockC); // ReferenceError: blockC is not defined
