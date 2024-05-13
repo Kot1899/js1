03d5b1ca14fcabb94e8d8bd8e03b9868cf65e426
Vitali Roma <romanenko.vit.kharkiv@gmail.com>
2024-05-13 18:26:37 +0300

    iter. of array

--- JS 2.0/M2/array.js
+++ JS 2.0/M2/array.js
@@ -1,2 +1,13 @@
+function enter(){
+    document.write("</br>"); }
+
+//array
 const array=[10,11,12];
-document.write(array[1]);
+document.write("array index 2 is ", array[1]);
+enter();
+
+//length array
+
+const age = [ 12 , 15 , 45 , 48 , 55];
+document.write("the length of array is ", age.length);
+enter();
